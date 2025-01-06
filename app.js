969d458b950ab4deec5218cb94cbf9132e292038
iampawannn <pawonebhattarai@gmail.com>
2025-01-06 18:52:22 +1100

    create function unsettledAmount

---
+++ app.js
@@ -0,0 +1,7 @@
+let unsettledAmount = 60;
+
+function showUnsettledAmount(unsettledAmount){
+const unsettledAmountElement = document.getElementById("unsettled_amount");
+unsettledAmountElement.innerHTML = unsettledAmount;
+}
+showUnsettledAmount();
