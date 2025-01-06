d204f6b2b1ddc69c39f7e6135c2eee54ac2a5312
iampawannn <pawonebhattarai@gmail.com>
2025-01-06 19:16:42 +1100

    add showFriends function

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-let unsettledAmount = 60;
+let totalAmount = 60;
+let friends = [
+    {
+    firstName: 'Alex',
+    lastName: 'Lee',
+    mobile: 5678908993,
+    email: 'alexlee@example.com', 
+    },
+    {
+     firstName: 'Bob',
+     lastName: 'Lyon',
+     mobile: 567888993,
+     email: 'boblyon@example.com', 
+        },
+    {
+    firstName: 'Donald',
+    lastName: 'Trump',
+    mobile: 5673238993,
+    email: 'dtrump@example.com', 
+    },
+] ;
+
+//function to display friends
+function showFriends(friends) {
+const friendsElement = document.getElementById('friends');
+let friendListElements = '';
+for(let friend of friends) {
+    let friendElement = `<div title="${friend.firstName} ${friend.lastName}"></div>`;
+    friendListElements = friendListElements + friendElement;
+    
+}
+friendsElement.innerHTML = friendListElements;
+}
+
+
 
 function showUnsettledAmount(unsettledAmount){
 const unsettledAmountElement = document.getElementById("unsettled_amount");
 unsettledAmountElement.innerHTML = unsettledAmount;
 }
-showUnsettledAmount();
+showFriends(friends);
+showUnsettledAmount(totalAmount / friends.length);
